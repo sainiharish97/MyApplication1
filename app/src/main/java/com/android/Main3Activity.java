6d7144f62f7dfaa0fbe0f3f5c73ac43bd50c014d
sainiHarish97 <jp.saini1996@gmail.com>
2017-05-18 17:01:57 -0400

    Initial commit

---
+++ app/src/main/java/com/android/Main3Activity.java
@@ -0,0 +1,15 @@
+package com.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.a1691703.myapplication.R;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
